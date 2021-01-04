02b55efd810f216c1818c11596c2acfc937734ed
Kafka <aiya0422@gmail.com>
2021-01-04 11:13:15 +0900

    create name saving

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,14 +30,14 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    //she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
 }
 
 function init() {
-    loadName();
+  loadName();
 }
 
 init();
